a9cd132d7952085e8284d199c80345674aba82a3
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:55:07 +0300

    Update task04.java

--- sprint01/task04.java
+++ sprint01/task04.java
@@ -1,3 +1,14 @@
+/*
+ * Create a new version of the Circle class where the draw method will be overloaded four times:
+ * The version without parameters.
+ * Using the one parameter color of String type.
+ * Using the one parameter scale of float type.
+ * Using two parameters color and scale of String and float type.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 class Circle {
     private double radius;
     
