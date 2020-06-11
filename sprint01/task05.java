86ff63da75d59ff2a32e13775d93fcc320e76040
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:56:11 +0300

    Update task05.java

--- sprint01/task05.java
+++ sprint01/task05.java
@@ -1,3 +1,16 @@
+/*
+ * A Point class, which models a 2D point with x and y coordinates should contains:
+ * Two private instance variables x and y  of int type.
+ * A constructor that constructs a point with the given x and y coordinates.
+ * A method getXYPair() which returns the x and y in a 2-element int array.
+ * A method called distance(int x, int y) that returns the distance from this point to another point at the given (x, y) coordinates.
+ * An overloaded distance(Point point) method that returns the distance from this point to the given Point instance.
+ * Another overloaded distance() method that returns the distance from this point to the origin (0, 0)
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 class Point {
     private int x;
     private int y;
