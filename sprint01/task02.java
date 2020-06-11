a27d2d95a0e89f9cef702c8363c5e7e0be753d74
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:52:23 +0300

    Update task02.java

--- sprint01/task02.java
+++ sprint01/task02.java
@@ -1,3 +1,14 @@
+/*
+ * 1. Create an instances of Employee class named 'emp1' and 'emp2'.
+ * 2. Set not null values for 'fullName' and 'salary' properties.
+ * 3. Create array of Employee type with name 'employees' and add two objects created before.
+ * 4. Create variable with name 'employeesInfo' of String type.
+ * 5. Using a loop, iterrate across array and write to variable named 'employeesInfo' info about each employee in next fommat:
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 Employee emp1 = new Employee();
 emp1.fullName = "John Doe";
 emp1.salary = 10000;
