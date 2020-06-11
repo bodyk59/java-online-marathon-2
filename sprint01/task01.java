5da09ead327a1e0bfc4c175e231adabee948a626
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:51:08 +0300

    Update task01.java

--- sprint01/task01.java
+++ sprint01/task01.java
@@ -1,3 +1,10 @@
+/*
+ * Create a Pet class with public fields name of type String and age of type int that are not initialized.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 class Pet {
     public String name;
     public int age;
