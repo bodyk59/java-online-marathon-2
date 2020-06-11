e365fce0bfd129d7eed86a2563f52062f4e5193a
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:57:17 +0300

    Update task06.java

--- sprint01/task06.java
+++ sprint01/task06.java
@@ -1,3 +1,15 @@
+/* 
+ * Create a Product class with fields name of type String and price of type double.
+ * The Product class must meet the following principles:
+ * All class fields must be private.
+ * Get and set methods must be used to access the class fields.
+ * The class must have a constructors with and without parameters.
+ * In the Product class write count() static method that returns count of created objects of Product type.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 class Product {
     private String name;
     private double price;
