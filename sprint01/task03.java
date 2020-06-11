a98e61efebd730d31df68df8b50a5f87ab9b2c12
Bohdan Kurchak <bogdan.kurchak@icloud.com>
2020-06-11 10:53:30 +0300

    Update task03.java

--- sprint01/task03.java
+++ sprint01/task03.java
@@ -1,3 +1,13 @@
+/*
+ * Create a new version of the Employee class that correspond next principles: 
+ * All class fields must be private.
+ * Get and set methods must be used to access the class fields.
+ * The class must have a constructors with and without parameters.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
 class Employee {
     private String fullName;
     private float salary;
